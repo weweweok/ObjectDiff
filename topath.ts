e73e08e99fbc753fb07b451ff8551cd89d067332
wewewe-ok <combination.kyoukyou@gmail.com>
2023-09-29 16:00:24 +0900

    不要なファイルを削除

--- topath.ts
+++
@@ -1,6 +0,0 @@
-import toPath from "https://esm.sh/underscore/modules/toPath.js";
-import { getAccentPhrases } from "./main.ts";
-
-const accent_phrases = await getAccentPhrases("こんにちは、ずんだもんです");
-
-console.log(toPath(accent_phrases, "text"));
